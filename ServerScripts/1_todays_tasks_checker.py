9fe99a161f5b714330596187bfca05cd01ceb0a2
Rushi-04 <borkarananta028@gmail.com>
2025-07-19 23:28:00 +0530

    fix bug #21

--- ServerScripts/1_todays_tasks_checker.py
+++ ServerScripts/1_todays_tasks_checker.py
@@ -123,7 +123,7 @@ def build_html_email(task_data):
     <body>  
         <p><strong>Scheduled Tasks for Today:</strong></p>  
         <table>
-            <tr><th>Task Name</th><th>Trigger Time</th></tr>
+            <tr><th>test</th><th>Trigger Time</th></tr>
     """   
     for task_name, trigger in task_data:
         html += f"<tr><td>{task_name}</td><td>{trigger}</td></tr>"
