d9ba5cfcb2094f804cac8212175d1f656d137ab2
Rushi-04 <borkarananta028@gmail.com>
2025-07-19 23:29:39 +0530

    fix bug #21

--- ServerScripts/1_todays_tasks_checker.py
+++ ServerScripts/1_todays_tasks_checker.py
@@ -151,7 +151,7 @@ def send_email(html_body):
 
 
 if __name__ == "__main__":
-    # folders = ["DevTasks", "EDI Tasks"]  
+    folders = ["DevTasks", "EDI Tasks"]  
     # folders = ["Microsoft\\Windows\\Application Experience"]
     all_tasks = []  
   
