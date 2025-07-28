c154a7ca007ae2c6e5b98bcce68d584c741c2c27
Rushi-04 <borkarananta028@gmail.com>
2025-07-28 23:15:33 +0530

    improved code

--- ServerScripts/1_todays_tasks_checker.py
+++ ServerScripts/1_todays_tasks_checker.py
@@ -75,7 +75,7 @@ def list_tasks_with_readable_triggers(subfolder_path):
     for part in subfolder_path.split("\\"):
         folder = folder.GetFolder(part)
   
-    tasks = folder.GetTasks(0)
+    tasks = folder.GetTasks(0)  
     results = []
    
     for t in tasks:
