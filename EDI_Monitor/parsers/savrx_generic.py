b81c1437ccd8e085c33f2d6182cb1addee8e139e
Rushikesh Borkar <borkarananta028@gmail.com>
2026-02-03 20:00:41 +0530

    fixed parser code

--- EDI_Monitor/parsers/savrx_generic.py
+++ EDI_Monitor/parsers/savrx_generic.py
@@ -1,22 +1,14 @@
 import re
-from datetime import datetime, timedelta
+from utils import today_yymmdd
 from .base import TraceParser
 
 class SavrxGenericParser(TraceParser):
     def __init__(self, base_dir, prefix, ext, trace_filename="trace.txt"):
         super().__init__(base_dir, trace_filename=trace_filename)
-        # Pattern: Prefix + (YYMMDD | YYMMDD-1) + Digits + Ext
-        
-        today = datetime.now()
-        yesterday = today - timedelta(days=1)
-        
-        t_str = today.strftime("%y%m%d")
-        y_str = yesterday.strftime("%y%m%d")
-        
-        # Regex: prefix + (today|yesterday) + digits + ext
-        # We use re.escape(ext) for safety.
-        self.pattern = re.compile(rf"{prefix}({t_str}|{y_str})\d+{re.escape(ext)}", re.IGNORECASE)
-        print(f"SavrxGeneric: Searching for pattern: {self.pattern.pattern}")
+        # Pattern: Prefix + YYMMDD + Digits + Ext
+        # Example: OEWEDI_260119162000.pgp
+        self.token = prefix + today_yymmdd()
+        self.pattern = re.compile(rf"{self.token}\d+{ext}", re.IGNORECASE)
 
     def find_file_in_line(self, line):
         m = self.pattern.search(line)
